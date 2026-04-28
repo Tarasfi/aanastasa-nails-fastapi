3df2632565edce5d09c801a9fdb7149d727a74d0
Tarasfi <tarasfirko21@gmail.com>
2026-04-29 01:46:01 +0200

    fixed: delete endpoints

--- app/routers/bookings.py
+++ app/routers/bookings.py
@@ -55,12 +55,12 @@ async def update_booking_status(db: db_dependency, booking_status_update: Bookin
 
 ############################# Should add is_active = False instead of that #############################
 #Delete booking
-# @router.delete("/bookings/{booking_id}", status_code=status.HTTP_204_NO_CONTENT)
-# async def delete_booking(db: db_dependency, booking_id: int):
-#     booking_to_delete = db.query(Bookings).filter(Bookings.id == booking_id).first()
-#     if booking_to_delete is None:
-#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Booking not found')
-#
-#     db.delete(booking_to_delete)
-#     db.commit()
+@router.delete("/bookings/{booking_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_booking(db: db_dependency, booking_id: int = Path(gt=0)):
+    booking_to_delete = db.query(Bookings).filter(Bookings.id == booking_id).first()
+    if booking_to_delete is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Booking not found')
+
+    db.delete(booking_to_delete)
+    db.commit()
 
