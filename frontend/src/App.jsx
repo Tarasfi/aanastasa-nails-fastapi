c46899ddd3a6b25d07ec9c423a27c664b0fabe99
Tarasfi <tarasfirko21@gmail.com>
2026-07-22 23:50:42 +0200

    Header add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { getServices } from './api/servicesApi';
+import React from 'react';
+import Header from './components/layout/Header';
+import ServiceList from './components/services/ServiceList';
 
 function App() {
-  const [services, setServices] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    
-    getServices()
-      .then((data) => {
-        setServices(data); 
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Помилка завантаження сервісів:", error);
-        setLoading(false);
-      });
-  }, []); 
-
-  if (loading) return <h2>Завантаження сервісів...</h2>;
-  
   return (
     <div>
-      <h1>Послуги салонa</h1>
-      
-      {/* Services */}
-      <ul>
-        {services.map((service) => (
-          <li key={service.id}>
-            {service.name} {service.description && `- ${service.description}`} - {service.price} грн - {service.duration_minutes} хв
-          </li>
-        ))}
-      </ul>
+      <Header />
+      <main>
+        <ServiceList/>
+      </main>
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/services/ServiceList.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { getServices } from '../../api/servicesApi'; 
+import ServiceCard from './ServiceCard';
+
+function ServiceList() {
+  const [services, setServices] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getServices()
+      .then((data) => {
+        setServices(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Помилка завантаження сервісів:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <h2>Завантаження сервісів...</h2>;
+
+  return (
+    <div>
+      <h2>Послуги салону</h2>
+      <ul>
+        {services.map((service) => (
+          <ServiceCard key={service.id} service={service} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ServiceList;
